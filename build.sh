f591b132f03772054222885c8bdd1b28f8b86d58
Andrew Grosser <dioptre@gmail.com>
2024-06-26 22:25:48 -0700

    wip

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 pip install -r requirements.txt
 python setup.py install
 python setup.py bdist_wheel
+#python -m pip install dist/cassandra_driver-3.29.1-cp310-cp310-macosx_13_0_arm64.whl --force-reinstall
 #DO NOT USE
 #poetry export -o requirements.txt --without-urls --without-hashes --without-urls --without-hashes
