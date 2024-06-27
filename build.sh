58405db15800b14c76cb69518213a557b03c8543
Andrew Grosser <dioptre@gmail.com>
2024-06-26 21:51:22 -0700

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+
+#poetry shell (run this from the ml/src directory)
 pip install -r requirements.txt
-#poetry export -o requirements.txt --without-urls --without-hashes --without-urls --without-hashes
 python setup.py install
 python setup.py bdist_wheel
+#DO NOT USE
+#poetry export -o requirements.txt --without-urls --without-hashes --without-urls --without-hashes
